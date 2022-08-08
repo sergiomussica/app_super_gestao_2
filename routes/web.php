03589a64fdcff5dbc7cfa25cb9b3b5e46993f020
sergiomussica <68793960+sergiomussica@users.noreply.github.com>
2022-08-08 13:17:48 +0200

    Aula 35,36,37
    Criação de Rotas
    Agrupamento de rotas
    Dar nome a rotas e chamar rotas com base no nome nas views

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,17 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/', 'PrincipalController@principal');
-Route::get('/sobre-nos', 'SobreNosController@sobrenos');
-Route::get('/contacto','ContactoController@contacto');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobrenos')->name('site.sobrenos');
+Route::get('/contacto','ContactoController@contacto')->name('site.login');
 
+Route::prefix('/app')->group( function(){
+
+    Route::get('/clientes', function(){return 'clientres';})->name('app.clientes');
+    Route::get('/fornecedores', function(){return 'fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos',function(){return 'produtos';})->name('app.produtos');
+});
+/*
 Route::get(
     '/contacto/{nome}/{categoria_id}/',
     function (
@@ -31,7 +38,7 @@ Route::get(
     echo "Estamos aqui: $nome - $categoria";    
     }
 )->where('categoria_id','[0-9]+')->where('nome','[A-Za-z]+');
-
+*/
 //nome
 
 /*
