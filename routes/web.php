60c8f0df2d7e6d68fdd2c916fb093eb5c9bc95fb
sergiomussica <68793960+sergiomussica@users.noreply.github.com>
2022-08-08 13:51:44 +0200

    Aula 38
    Redirecçionamento de rotas com a função redirect e dentro de uma rota

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,17 @@ Route::prefix('/app')->group( function(){
     Route::get('/fornecedores', function(){return 'fornecedores';})->name('app.fornecedores');
     Route::get('/produtos',function(){return 'produtos';})->name('app.produtos');
 });
+
+Route::get('/rota1', function(){
+    echo 'Rota 1';
+})->name('site.rota1');
+
+Route::get('/rota2', function(){
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+//Route::redirect('/rota2','/rota1');
+
 /*
 Route::get(
     '/contacto/{nome}/{categoria_id}/',
