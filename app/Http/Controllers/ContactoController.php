062e8c5d113a133b4cc61ff00c86c7bde259de2c
sergiomussica <68793960+sergiomussica@users.noreply.github.com>
2022-08-20 19:03:10 +0200

    Aula 121 - Gravando dados do Formulário Contacto
    Inserir dados na tabela
    
    //Preencher  um object manualmente
    $contacto = new SiteContacto();
    $contacto->nome = $request->input('nome');
    $contacto->save();
    
    //método create NB: preencher o $fillable
    %$contacto->create($request0>all());
    
    //método fill Nb: preencher fillable
    $contacto->fill($request->all());
    $save();

--- app/Http/Controllers/ContactoController.php
+++ app/Http/Controllers/ContactoController.php
@@ -1,14 +1,32 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\SiteContacto;
 use Illuminate\Http\Request;
 
 class ContactoController extends Controller
 {
     //
-    public function contacto(){
-        var_dump($_GET);
+    public function contacto(Request $request){
+       
+        /*$contacto = new SiteContacto();
+        $contacto->nome = $request->input('nome');
+        $contacto->telefone = $request->input('telefone');
+        $contacto->email = $request->input('email');
+        $contacto->motivo_contato = $request->input('motivo_contato');
+        $contacto->mensagem = $request->input('mensagem');
+        $contacto->save();*/
+
+        //print_r($contacto->getAttributes());
+
+        $contacto = new SiteContacto();
+        $contacto->create($request->all());
+
+
+
+        //$contacto->fill($request->all());
+        //$contacto->save();
+    
         return view('site.contacto',['titulo'=>'Contacto']);
     }
 }
--- app/SiteContacto.php
+++ app/SiteContacto.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class SiteContacto extends Model
 {
     //
+    protected $fillable = ['nome','telefone','email','motivo_contato','mensagem'];
 }
