a936ed71522cc918311d2baca8559cdbceec637b
sergiomussica <68793960+sergiomussica@users.noreply.github.com>
2022-08-20 13:05:18 +0200

    adequação do projecto a realidade do curso

---
+++ app/Fornecedor.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Fornecedor extends Model
+{
+    //
+    //
+    use SoftDeletes;
+
+    protected $table = 'fornecedores';
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
+}
