d09e0eaa04418082b8fda2916a015b2138de2971
sergiomussica <68793960+sergiomussica@users.noreply.github.com>
2022-08-23 15:23:58 +0200

    134 Customização de feedback de validação
     $regras =             [
                'nome' => 'required|min:3|max:40|unique:site_contatos',
                'telefone' => 'required',
                'email' => 'email',
                'motivo_contatos_id' => 'required',
                'mensagem' => 'required|max:2000'
            ];
    
            $feedback = [
                'nome.min' => 'O campo nome precisa ter no mínimo 3 caracteres',
                'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
                'nome.unique' => 'O nome informado já está em uso',
    
                'email.email' => 'O email informado não é válido',
    
                'mensagem.max' => 'A mensagem deve ter no máximo 2000 caracteres',
    
                'required' => 'O campo :attribute deve ser preenchido'
            ];
    
            $request->validate($regras, $feedback);

--- app/Http/Controllers/ContactoController.php
+++ app/Http/Controllers/ContactoController.php
@@ -34,14 +34,28 @@ class ContactoController extends Controller
     }
 
     public function salvar(Request $request){
-        $request->validate([
-            'nome' => 'required|min:3|max:40:unique:site_contactos',
+        $regras =             [
+            'nome' => 'required|min:3|max:40|unique:site_contatos',
             'telefone' => 'required',
             'email' => 'email',
             'motivo_contatos_id' => 'required',
-            'mensagem' => 'required|max:2000',
+            'mensagem' => 'required|max:2000'
+        ];
 
-        ]);
+        $feedback = [
+            'nome.min' => 'O campo nome precisa ter no mínimo 3 caracteres',
+            'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
+            'nome.unique' => 'O nome informado já está em uso',
+
+            'email.email' => 'O email informado não é válido',
+
+            'mensagem.max' => 'A mensagem deve ter no máximo 2000 caracteres',
+
+            'required' => 'O campo :attribute deve ser preenchido'
+        ];
+
+        $request->validate($regras, $feedback);
+
         SiteContacto::create($request->all());
         return redirect()->route('site.index');
     }
