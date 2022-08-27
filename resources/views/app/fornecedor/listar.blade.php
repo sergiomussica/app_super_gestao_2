5f834795f229ee0bf1f408c0aa2436dd24407230
sergiomussica <68793960+sergiomussica@users.noreply.github.com>
2022-08-27 18:44:31 +0200

    157
     <!--
                    <br>
                    {{ $fornecedores->count() }} - Total de registros por página
                    <br>
                    {{ $fornecedores->total() }} - Total de registros da consulta
                    <br>
                    {{ $fornecedores->firstItem() }} - Número do primeiro registro da página
                    <br>
                    {{ $fornecedores->lastItem() }} - Número do último registro da página
    
                    -->
                    <br>
                    Exibindo {{ $fornecedores->count() }} fornecedores de {{ $fornecedores->total() }} (de {{ $fornecedores->firstItem() }} a {{ $fornecedores->lastItem() }})

--- resources/views/app/fornecedor/listar.blade.php
+++ resources/views/app/fornecedor/listar.blade.php
@@ -47,7 +47,19 @@
 
                 {{ $fornecedores->appends($request)->links() }}
 
-               
+                <!--
+                <br>
+                {{ $fornecedores->count() }} - Total de registros por página
+                <br>
+                {{ $fornecedores->total() }} - Total de registros da consulta
+                <br>
+                {{ $fornecedores->firstItem() }} - Número do primeiro registro da página
+                <br>
+                {{ $fornecedores->lastItem() }} - Número do último registro da página
+
+                -->
+                <br>
+                Exibindo {{ $fornecedores->count() }} fornecedores de {{ $fornecedores->total() }} (de {{ $fornecedores->firstItem() }} a {{ $fornecedores->lastItem() }})
             </div>
         </div>
 
