14f2e0566b5bf5a8e7acb2f1033ae7fa154c4477
sergiomussica <68793960+sergiomussica@users.noreply.github.com>
2022-08-27 23:29:12 +0200

    167 cadastro de produtos edit

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -81,7 +81,8 @@ class ProdutoController extends Controller
      */
     public function edit(Produto $produto)
     {
-        //
+        $unidades = Unidade::all();
+        return view('app.produto.edit', ['produto' => $produto, 'unidades' => $unidades]);
     }
 
     /**
