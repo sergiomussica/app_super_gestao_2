ecb926ee129c267ded97e6112c640d8d6bfea913
sergiomussica <68793960+sergiomussica@users.noreply.github.com>
2022-08-27 14:27:32 +0200

    155

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -25,7 +25,8 @@ class FornecedorController extends Controller
 
         $msg = '';
 
-        if($request->input('_token') != '') {
+        //inclusão
+        if($request->input('_token') != '' && $request->input('id') == '') {
             //validacao
             $regras = [
                 'nome' => 'required|min:3|max:40',
@@ -53,6 +54,29 @@ class FornecedorController extends Controller
             //dados view
             $msg = 'Cadastro realizado com sucesso';
         }
+
+        //edição
+        if($request->input('_token') != '' && $request->input('id') != '') {
+            $fornecedor = Fornecedor::find($request->input('id'));
+            $update = $fornecedor->update($request->all());
+
+            if($update) {
+                $msg = 'Atualização realizado com sucesso';
+            } else {
+                $msg = 'Erro ao tentar atualizar o registro';
+            }
+
+            return redirect()->route('app.fornecedor.editar', ['id' => $request->input('id'), 'msg' => $msg]);
+        }
+
         return view('app.fornecedor.adicionar', ['msg' => $msg]);
     }
+
+    public function editar($id, $msg = '') {
+
+        $fornecedor = Fornecedor::find($id);
+
+        return view('app.fornecedor.adicionar', ['fornecedor' => $fornecedor, 'msg' => $msg]);
+    }
+    
 }
