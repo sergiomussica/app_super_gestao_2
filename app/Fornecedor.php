80b88bf0d0b6399b3ab92f1ddaf1a0e446999432
sergiomussica <68793960+sergiomussica@users.noreply.github.com>
2022-08-29 00:02:13 +0200

    185 1 para n has many

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -5,12 +5,19 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+//fornecedors
+//fornecedores
+
 class Fornecedor extends Model
 {
     //
-    //
     use SoftDeletes;
-
+    
     protected $table = 'fornecedores';
     protected $fillable = ['nome', 'site', 'uf', 'email'];
+    
+    public function produtos() {
+        return $this->hasMany('App\Item', 'fornecedor_id', 'id');
+        //return $this->hasMany('App\Item');
+    }
 }
--- resources/views/app/fornecedor/listar.blade.php
+++ resources/views/app/fornecedor/listar.blade.php
@@ -3,7 +3,7 @@
 @section('titulo', 'Fornecedor')
 
 @section('conteudo')
-
+    
     <div class="conteudo-pagina">
 
         <div class="titulo-pagina-2">
@@ -29,10 +29,10 @@
                             <th></th>
                             <th></th>
                         </tr>
-                    </thead>
+                    </head>
 
                     <tbody>
-                        @foreach ($fornecedores as $fornecedor)
+                        @foreach($fornecedores as $fornecedor)
                             <tr>
                                 <td>{{ $fornecedor->nome }}</td>
                                 <td>{{ $fornecedor->site }}</td>
@@ -41,10 +41,32 @@
                                 <td><a href="{{ route('app.fornecedor.excluir', $fornecedor->id) }}">Excluir</a></td>
                                 <td><a href="{{ route('app.fornecedor.editar', $fornecedor->id) }}">Editar</a></td>
                             </tr>
+                            <tr>
+                                <td colspan="6">
+                                    <p>Lista de produtos</p>
+                                    <table border="1" style="margin:20px">
+                                        <thead>
+                                            <tr>
+                                                <th>ID</th>
+                                                <th>Nome</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach($fornecedor->produtos as $key => $produto)
+                                                <tr>
+                                                    <td>{{ $produto->id}}</td>
+                                                    <td>{{ $produto->nome}}</td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </td>
+                            <tr>
                         @endforeach
                     </tbody>
                 </table>
 
+                
                 {{ $fornecedores->appends($request)->links() }}
 
                 <!--
@@ -66,3 +88,4 @@
     </div>
 
 @endsection
+
