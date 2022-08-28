d666157567efe5aa0e79869e30673841c79bc6f1
sergiomussica <68793960+sergiomussica@users.noreply.github.com>
2022-08-28 11:23:13 +0200

    173
    php artisan make:model ProdutoDetalhe
    php artisan make:controller --resource ProdutoDetalhecontroller

--- resources/views/produto/create.blade.php
+++ resources/views/produto/create.blade.php
@@ -7,7 +7,7 @@
     <div class="conteudo-pagina">
 
         <div class="titulo-pagina-2">
-            <p>Editar Produto</p>
+            <p>Adicionar Produto</p>
         </div>
 
         <div class="menu">
@@ -19,8 +19,8 @@
 
         <div class="informacao-pagina">
             <div style="width: 30%; margin-left: auto; margin-right: auto;">
-                @component('app.produto._components.form_create_edit', ['produto' => $produto, 'unidades' => $unidades])
-                @endcomponent
+                @component('app.produto._components.form_create_edit', ['unidades' => $unidades])
+                @endcomponent             
             </div>
         </div>
 
