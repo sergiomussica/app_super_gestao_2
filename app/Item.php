7c1396c221d8bbe7b06518dabd1ee56d7c34cc0b
sergiomussica <68793960+sergiomussica@users.noreply.github.com>
2022-08-28 22:56:15 +0200

    183 1 para N exibindo informação de fornecedor

--- app/Item.php
+++ app/Item.php
@@ -6,12 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    //
-
     protected $table = 'produtos';
     protected $fillable = ['nome', 'descricao', 'peso', 'unidade_id'];
 
     public function itemDetalhe() {
         return $this->hasOne('App\ItemDetalhe', 'produto_id', 'id');
+    }
+
+    public function fornecedor() {
+        return $this->belongsTo('App\Fornecedor');
     }
 }
--- resources/views/app/produto/index.blade.php
+++ resources/views/app/produto/index.blade.php
@@ -24,6 +24,7 @@
                         <tr>
                             <th>Nome</th>
                             <th>Descrição</th>
+                            <th>Fornecedor</th>
                             <th>Peso</th>
                             <th>Unidade ID</th>
                             <th>Comprimento</th>
@@ -40,6 +41,7 @@
                             <tr>
                                 <td>{{ $produto->nome }}</td>
                                 <td>{{ $produto->descricao }}</td>
+                                <td>{{ $produto->fornecedor->nome }}</td>
                                 <td>{{ $produto->peso }}</td>
                                 <td>{{ $produto->unidade_id }}</td>
                                 <td>{{ $produto->itemDetalhe->comprimento ?? '' }}</td>
@@ -59,7 +61,6 @@
                         @endforeach
                     </tbody>
                 </table>
-
                 
                 {{ $produtos->appends($request)->links() }}
 
