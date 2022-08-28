f050bf23cc60b2281eddaecf2630b35ece4bb245
sergiomussica <68793960+sergiomussica@users.noreply.github.com>
2022-08-28 20:18:07 +0200

    177 Eloquente ORM 1 para 1 - Estabelecendo relacionamento(hasOne)

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -18,6 +18,7 @@ class ProdutoController extends Controller
     {
         $produtos = Produto::paginate(10);
 
+        /*
         foreach($produtos as $key => $produto) {
             //print_r($produto->getAttributes());
             //echo '<br><br>';
@@ -34,6 +35,7 @@ class ProdutoController extends Controller
             }
             //echo '<hr>';
         }
+        */
 
         return view('app.produto.index', ['produtos' => $produtos, 'request' => $request->all() ]);
     }
